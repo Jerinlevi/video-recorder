f200cd13f4fc05cee105e056caa82acd4ba07242
Jerin J <146941032+Jerinlevi@users.noreply.github.com>
2025-03-30 13:10:41 +0530

    script.js

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let video=document.querySelector("video");
 let recordBtnCont=document.querySelector(".record-btn-cont")
 let recordBtn=document.querySelector(".record-btn")
 let recordFlag=false;
+let captureBtnele=document.querySelector(".capture-btn-cont")
+let captureBtn=document.querySelector(".capture-btn")
+let transparent="transparent";
 let recorder;//store undefined
 let chunks=[];//media data are stored in a chunks
 let constraints={
@@ -33,13 +36,68 @@ navigator.mediaDevices.getUserMedia(constraints)
         recordFlag=!recordFlag;
         if(recordFlag){//start
             recorder.start();
-            recordBtn.classList.add("scale-record")
+            recordBtn.classList.add("scale-record");
+            startTimer();
 
         }else{
             recorder.stop();
             recordBtn.classList.remove("scale-record");
+            stopTimer();
         }
     })
+});
+
+
+captureBtnele.addEventListener("click",(e)=>{
+    captureBtn.classList.add("scale-capture");
+    let canvas=document.createElement("canvas");
+    canvas.width=video.videoWidth;
+    canvas.height=video.videoHeight;
+    let imageUrl=canvas.toDataURL("image/jpeg",0.1);
+    let getting=canvas.getContext("2d");
+    getting.drawImage(video,0,0,canvas.width,canvas.height);
+    getting.fillStyle=transparent;
+    getting.fillRect(0,0,canvas.width,canvas.height);
+    let a =document.createElement('a');
+    a.href=imageUrl;
+    a.download="stream.jpeg"
+    a.click();
+    setTimeout(()=>{
+        captureBtn.classList.remove("scale-capture");
+
+    },500);
 })
 
-
+let timerId;
+let counter=0;
+let timer=document.querySelector(".timer")
+function startTimer(){
+    timer.style.display="block";
+    function displayTimer(){
+        let totalSeconds=counter;
+        let hours=Number.parseInt(totalSeconds/3600);
+        totalSeconds=totalSeconds%3600;
+        let minutes=Number.parseInt(totalSeconds/60);
+        totalSeconds=totalSeconds%60;
+        let seconds=totalSeconds;
+        hours=(hours < 10) ?`0${hours}`:hours;
+        minutes=(minutes < 10) ?`0${minutes}`:minutes;
+        seconds=(seconds < 10) ?`0${seconds}`:seconds;
+        timer.innerText=`${hours}:${minutes}:${seconds}`;
+        counter++;
+    }
+    timerId=setInterval(displayTimer,1000);
+}
+function stopTimer(){
+     clearInterval(timerId);
+     timer.style.display="none";
+     counter=0;
+}
+let filter=document.querySelector(".filter1")
+let all=document.querySelectorAll(".filter");
+all.forEach((filterElem)=>{
+    filterElem.addEventListener("click",(e)=>{
+        transparent=getComputedStyle(filterElem).getPropertyValue("background-color");
+        filter.style.backgroundColor=transparent;
+    })
+});
